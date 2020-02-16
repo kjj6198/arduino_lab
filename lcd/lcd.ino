992e52061e48f1ba86ca5c8c618e2f3e17b367e5
kjj6198 <kjj6198@gmail.com>
2020-02-16 23:37:10 +0900

    Add Keypad

--- lcd/lcd.ino
+++ lcd/lcd.ino
@@ -1,6 +1,22 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <LiquidCrystal.h>
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char hexaKeys[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'},
+};
+
+byte rowPins[ROWS] = {39, 41, 43, 45}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {47, 49, 51, 53}; //connect to the column pinouts of the keypad
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 RTC_DS1307 rtc;
 
@@ -22,13 +38,14 @@ void setup()
   {
     Serial.println("RTC lost power, lets set the time!");
     // set to computer time
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 }
 
 void loop()
 {
   DateTime now = rtc.now();
+
   char buffer[9] = "";
   lcd.setCursor(0, 0);
   sprintf(buffer, "%02d/%02d/%02d", now.year(), now.month(), now.day());
@@ -36,4 +53,17 @@ void loop()
   lcd.setCursor(0, 1);
   sprintf(buffer, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
   lcd.print(buffer);
+
+  char customKey = customKeypad.getKey();
+  if (customKey)
+  {
+    Serial.println(customKey);
+  }
+
+  if (now == DateTime(2020, 2, 16, 23, 4, 00))
+  {
+    lcd.clear();
+    lcd.print("times up!!");
+    delay(1000);
+  }
 }
